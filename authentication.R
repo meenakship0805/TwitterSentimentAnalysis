eb3f715524dd243243cac208cacc15e76efde989
meenakship0805 <meenakship0805@gmail.com>
2025-04-22 17:21:42 +0530

    authentication.R

--- authentication.R
+++ authentication.R
@@ -1,14 +1,19 @@
-library(ROAuth)
-library(twitteR)
+# Install rtweet package if not already installed
+if (!require("rtweet")) install.packages("rtweet")
 
-consumer_key <-"AKJsxNqX2D8uTo9orgjRirvWL"
-consumer_secret <- "QOKk0ctHhbXNQ5QaipqofrZQzWM92mfkcoP60xe7HJzjSUCz6F"
-access_token<-"2617540074-5l6gGJhCP8iw9DS7sVD9qsFaUGfWGO9fqlHt5Wg"
-access_secret <- "VVMfNIzgPEUmCk5QyIWr5A4ZSC2Lxy7CERoUtWs4jAe0l"
+# Load the rtweet library
+library(rtweet)
 
- setup_twitter_oauth(consumer_key ,consumer_secret, access_token,  access_secret )
- 
-cred <- OAuthFactory$new(consumerKey='AKJsxNqX2D8uTo9orgjRirvWL', consumerSecret='QOKk0ctHhbXNQ5QaipqofrZQzWM92mfkcoP60xe7HJzjSUCz6F',requestURL='https://api.twitter.com/oauth/request_token',accessURL='https://api.twitter.com/oauth/access_token',authURL='https://api.twitter.com/oauth/authorize')
+# Create Twitter token using OAuth 1.0a method
+token <- rtweet::create_token(
+  app = "meenakshi", 
+  consumer_key = "km39WzDubVP1ISYdB0jeqXutI",
+  consumer_secret = "gNVwALjibc7aD9bnHwEcyH2ejYLunVV4IqopxYViE2o4t1vbZv",
+  access_token = "1809615184484528132-6Lt70FHM32TL6idjiVWcAzuqGuzgUa",
+  access_secret = "K4rT7o1YBJDCkcXki5hX19s20RoWyjuZq3apjtgAvT7dQ"
+)
 
-cred$handshake(cainfo="cacert.pem")
-#After this you will be redirected to a URL where you click on authorize app and get the passkey to be entered in RStudio
+# ✅ Test by fetching latest tweets for a keyword
+tweets <- search_tweets("openai", n = 5, token = token)
+print(tweets)
+
